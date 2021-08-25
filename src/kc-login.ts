d5c29eaa35beb65c70710dca89299b7e570f4e9d
Sebastian Richter <sebastian.max.richter@gmail.com>
2021-08-25 13:14:53 +0200

    Add idpHint and scope param to login

--- src/kc-login.ts
+++ src/kc-login.ts
@@ -1,57 +1,73 @@
 import { getAuthCodeFromLocation } from "./utils";
 
-Cypress.Commands.add("kcLogin", (user: string) => {
-  Cypress.log({ name: "Login" });
+Cypress.Commands.add(
+  "kcLogin",
+  ({
+    user,
+    idpHint,
+    scope
+  }: {
+    user: string;
+    idpHint?: string;
+    scope?: string;
+  }) => {
+    Cypress.log({ name: "Login" });
 
-  cy.fixture(`users/${user}`).then((userData: UserData) => {
-    const authBaseUrl = Cypress.env("auth_base_url");
-    const realm = Cypress.env("auth_realm");
-    const client_id = Cypress.env("auth_client_id");
+    cy.fixture(`users/${user}`).then((userData: UserData) => {
+      const authBaseUrl = Cypress.env("auth_base_url");
+      const realm = Cypress.env("auth_realm");
+      const client_id = Cypress.env("auth_client_id");
 
-    cy.request({
-      url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/auth`,
-      followRedirect: false,
-      qs: {
-        scope: "openid",
-        response_type: "code",
-        approval_prompt: "auto",
-        redirect_uri: Cypress.config("baseUrl"),
-        client_id
-      }
-    })
-      .then(response => {
-        const html = document.createElement("html");
-        html.innerHTML = response.body;
-
-        const form = html.getElementsByTagName("form")[0];
-        const url = form.action;
-
-        return cy.request({
-          method: "POST",
-          url,
-          followRedirect: false,
-          form: true,
-          body: {
-            username: userData.username,
-            password: userData.password
-          }
-        });
+      cy.request({
+        url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/auth`,
+        followRedirect: false,
+        qs: {
+          scope: scope
+            ? scope.includes("openid")
+              ? scope
+              : ["openid", scope].join(" ")
+            : "opendid",
+          response_type: "code",
+          approval_prompt: "auto",
+          redirect_uri: Cypress.config("baseUrl"),
+          kc_idp_hint: idpHint,
+          client_id
+        }
       })
-      .then(response => {
-        const code = getAuthCodeFromLocation(response.headers["location"]);
+        .then(response => {
+          const html = document.createElement("html");
+          html.innerHTML = response.body;
 
-        cy.request({
-          method: "post",
-          url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/token`,
-          body: {
-            client_id,
-            redirect_uri: Cypress.config("baseUrl"),
-            code,
-            grant_type: "authorization_code"
-          },
-          form: true,
-          followRedirect: false
-        }).its("body");
-      });
-  });
-});
+          const form = html.getElementsByTagName("form")[0];
+          const url = form.action;
+
+          return cy.request({
+            method: "POST",
+            url,
+            followRedirect: false,
+            form: true,
+            body: {
+              username: userData.username,
+              password: userData.password
+            }
+          });
+        })
+        .then(response => {
+          const code = getAuthCodeFromLocation(response.headers["location"]);
+
+          cy.request({
+            method: "post",
+            url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/token`,
+            body: {
+              client_id,
+              redirect_uri: Cypress.config("baseUrl"),
+              code,
+              grant_type: "authorization_code"
+            },
+            form: true,
+            followRedirect: false
+          }).its("body");
+        });
+    });
+  }
+);
